5105767e583b57a9f702794e2661aca7ef867ff3
Ralph Sébastien Placide <placideralph@gmail.com>
2023-08-31 19:24:18 -0400

    feat: demo web scraping w puppeteer

--- index.ts
+++ index.ts
@@ -11,6 +11,27 @@ const getBooks = async () => {
   await page.goto('https://books.toscrape.com/', {
     waitUntil: 'domcontentloaded',
   });
+
+  // Get page data
+  const books = await page.evaluate(() => {
+    const bookList: NodeListOf<HTMLElement> =
+      document.querySelectorAll('.product_pod');
+    let result: any[] = [];
+    bookList.forEach((book, index) => {
+      const id = index;
+      const title = (book.querySelector('h3 > a') as HTMLElement)?.title;
+      const price = (book.querySelector('.product_price > p') as HTMLElement)
+        ?.innerText;
+      result.push({ id, title, price });
+    });
+    return result;
+  });
+
+  // Display the books
+  console.log(books);
+
+  // Close the browser
+  await browser.close();
 };
 
 getBooks();
